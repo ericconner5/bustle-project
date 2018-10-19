96544b4dcf5b1796961c1313714c0e2d3e01d55b
Eric Conner <theluvq@gmail.com>
2018-10-19 12:06:22 -0700

    adds a working img in nav-bar component

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -4,10 +4,24 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
+  template: `
+    <nav class="navbar">
+
+      <!-- logo -->
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="assets/hamburger-icon.png">
+        </a>
+      </div>
+    </nav>
+  `,
+  // styles: []
 })
 export class NavBarComponent implements OnInit {
-
   constructor() {
+    // public images = [
+    //   image: "assets.hamburger-icon.png",
+    // ]
    }
 
   ngOnInit() {
