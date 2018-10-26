470e2aadd9e3c05026fdf43ae61663b8bb39cf60
Eric Conner <theluvq@gmail.com>
2018-10-26 09:38:15 -0700

    add compontents for additional pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,48 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NewsComponent } from './news/news.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { BeautyComponent } from './beauty/beauty.component';
+import { FashionComponent } from './fashion/fashion.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { BooksComponent } from './books/books.component';
+import { TechComponent } from './tech/tech.component';
+import { FoodComponent } from './food/food.component';
+import { FeaturesComponent } from './features/features.component';
+import { VideoComponent } from './video/video.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AdvertiseComponent } from './advertise/advertise.component';
+import { AppsComponent } from './apps/apps.component';
+import { LabsComponent } from './labs/labs.component';
+import { TermsComponent } from './terms/terms.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { DmcaComponent } from './dmca/dmca.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    NewsComponent,
+    EntertainmentComponent,
+    BeautyComponent,
+    FashionComponent,
+    LifestyleComponent,
+    BooksComponent,
+    TechComponent,
+    FoodComponent,
+    FeaturesComponent,
+    VideoComponent,
+    AboutUsComponent,
+    AdvertiseComponent,
+    AppsComponent,
+    LabsComponent,
+    TermsComponent,
+    PrivacyComponent,
+    DmcaComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
